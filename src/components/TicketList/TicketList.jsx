27010e96d0db129e5890fbd0018994de389048f0
Maxim Valishevskiy <mvalishevskiy@only.com.ru>
2024-04-06 03:19:51 +0700

    init

--- src/components/TicketList/TicketList.jsx
+++ src/components/TicketList/TicketList.jsx
@@ -8,10 +8,13 @@ import {
 } from '../../features/tickets.js';
 import { format, parseISO } from 'date-fns';
 import pluralize from 'pluralize-ru/index.js';
+import {Empty, Spin} from "antd";
 
 export default function TicketList() {
   const tickets = ticketsFiltering([...useSelector(selectTickets)]);
   const [ticketsShow, setTicketsShow] = useState(5);
+  const loading = useSelector((state) => state.tickets.loading);
+  const error = useSelector((state) => state.tickets.error);
 
   function ticketsFiltering(tickets) {
     const order = useSelector(selectOrder);
@@ -25,12 +28,14 @@ export default function TicketList() {
           return 2;
         case '3 пересадки':
           return 3;
+        default:
+          return -1;
       }
     });
     let ticketsNew = tickets.filter((ticket) => {
-      const stops =
-        ticket.segments[0].stops.length + ticket.segments[1].stops.length;
-      return filter.includes(stops);
+      const stops1 = ticket.segments[0].stops.length;
+      const stops2 = ticket.segments[1].stops.length;
+      return filter.includes(stops1) || filter.includes(stops2);
     });
     return sortTickets(ticketsNew, order);
   }
@@ -88,57 +93,69 @@ export default function TicketList() {
   }
 
   return (
-    <>
-      {tickets.slice(0, ticketsShow).map((ticket) => (
-        <div className="ticket">
-          <div className="ticket__header">
-            <span className="ticket__header--price">{ticket.price} P</span>
-            <img
-              className="ticket__header--logo"
-              src={`//pics.avs.io/99/36/${ticket.carrier}.png`}
-              alt="logo"
-            />
-          </div>
-          {ticket.segments.map((segment) => (
-            <div className="ticket__section">
-              <div className="ticket__segment">
-                <div className="ticket__segment--title">
-                  {segment.origin} – {segment.destination}
-                </div>
-                <div className="ticket__segment--description">
-                  {formatFlightTime(segment.date, segment.duration)}
-                </div>
-              </div>
-              <div className="ticket__segment">
-                <div className="ticket__segment--title">В пути</div>
-                <div className="ticket__segment--description">
-                  {formatMinutes(segment.duration)}
-                </div>
-              </div>
-              <div className="ticket__segment">
-                <div className="ticket__segment--title">
-                  {pluralize(
-                    segment.stops.length,
-                    'Нет пересадок',
-                    '%d Пересадка',
-                    '%d Пересадки',
-                    '$d Пересадок',
-                  )}
-                </div>
-                <div className="ticket__segment--description">
-                  {segment.stops.join(', ')}
-                </div>
-              </div>
-            </div>
-          ))}
+  <>
+    {loading ? (
+        <div className="loader-container">
+          <Spin size="large" />
         </div>
-      ))}
-      <div
+    )  : tickets.length === 0 ? (
+        <div className="empty-container">
+          <Empty
+              description="Рейсов, подходящих под заданные фильтры, не найдено"
+          />
+        </div>
+    ) : (
+        tickets.slice(0, ticketsShow).map((ticket) => (
+            <div className="ticket">
+              <div className="ticket__header">
+                <span className="ticket__header--price">{ticket.price} P</span>
+                <img
+                    className="ticket__header--logo"
+                    src={`//pics.avs.io/99/36/${ticket.carrier}.png`}
+                    alt="logo"
+                />
+              </div>
+              {ticket.segments.map((segment) => (
+                  <div className="ticket__section">
+                    <div className="ticket__segment">
+                      <div className="ticket__segment--title">
+                        {segment.origin} – {segment.destination}
+                      </div>
+                      <div className="ticket__segment--description">
+                        {formatFlightTime(segment.date, segment.duration)}
+                      </div>
+                    </div>
+                    <div className="ticket__segment">
+                      <div className="ticket__segment--title">В пути</div>
+                      <div className="ticket__segment--description">
+                        {formatMinutes(segment.duration)}
+                      </div>
+                    </div>
+                    <div className="ticket__segment">
+                      <div className="ticket__segment--title">
+                        {pluralize(
+                            segment.stops.length,
+                            'Нет пересадок',
+                            '%d Пересадка',
+                            '%d Пересадки',
+                            '$d Пересадок',
+                        )}
+                      </div>
+                      <div className="ticket__segment--description">
+                        {segment.stops.join(', ')}
+                      </div>
+                    </div>
+                  </div>
+              ))}
+            </div>
+        ))
+    )}
+    {tickets.length !== 0 && (<div
         className="show-more"
         onClick={() => setTicketsShow(ticketsShow + 5)}
-      >
-        Показать еще 5 билетов!
-      </div>
-    </>
+    >
+      Показать еще 5 билетов!
+    </div>)}
+  </>
   );
 }
