180d6a4dc2ab03e8c250642b171a05298e55ba0b
Maxim Valishevskiy <mvalishevskiy@only.com.ru>
2024-04-05 23:26:32 +0700

    init

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <div className="logo-container">
       <img
-        src={window.location.origin + '/src/assets/Logo.png'}
+        src='src/assets/Logo.png'
         alt="Descriptive alternative text of the image"
         className="logo"
         loading="lazy"
